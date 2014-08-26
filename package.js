5d4d7023b1793595191da303da7e47e1c0cd2b34
Roshan Jobanputra <rosh93@gmail.com>
2014-08-26 11:56:26 -0700

    Upgrade package to support meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "Reactive filtering for client-side data powered by twitter typeahead.js bloodhound engine"
+  summary: "Reactive filtering for client-side data powered by twitter typeahead.js bloodhound engine",
+  version: "0.0.5",
+  git: "https://github.com/rosh93/meteor-dataset-filter.git"
 });
 
 Package.on_use(function(api, where) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use('jquery', 'client');
   api.add_files('bloodhound.js', 'client');
   api.add_files('dataset_filter.js', 'client');
